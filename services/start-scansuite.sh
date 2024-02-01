425c7b95779caf26400cc4fe98dec896a66eed95
cepxeo <cepxeo@gmail.com>
2024-02-01 10:51:30 +0000

    update

--- services/start-scansuite.sh
+++ services/start-scansuite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f /home/USER/scansuite-demo/docker-compose.yml up -d
+docker-compose -f /home/USER/scansuite-demo/docker-compose.yml up --scale worker=2 -d
 cd /home/USER/apps/django-DefectDojo
 ./dc-up-d.sh mysql-redis
