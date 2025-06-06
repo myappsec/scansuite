dd9aad0bf121e2a456547598e2e0fd0cb80326f9
Ubuntu <azureuser@scansuite-dev.3203h5pdmniuzj35fk2b1bclrf.ax.internal.cloudapp.net>
2025-06-06 18:28:32 +0000

    dojo path

--- services/start-scansuite.sh
+++ services/start-scansuite.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /USER/apps/scansuite && ./start-scansuite
-cd /USER/apps/django-DefectDojo && sudo docker compose up -d
+cd /USER/apps/scansuite/defectdojo && sudo docker compose up -d
